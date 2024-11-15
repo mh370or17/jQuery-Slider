1ddb7ca2c3961418280e128aa593e17180f4b12e
mh370or17 <e.jazz.mobilink@gmail.com>
2024-11-15 19:17:57 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,606 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script src="js/jquery.min.js"></script>
+<!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>--><!--479x476-->
+<script> 
+$(document).ready(function(){
+	//needed dots.
+	function first_dot(){
+  		if($('#images_main_container').has('image_container_1010')) {
+			$(".dot_0").css("background-color","black");
+			$(".dot_1").css("background-color","gold");
+			$(".dot_2").css("background-color","gold");
+			$(".dot_3").css("background-color","gold");
+			$(".dot_4").css("background-color","gold");
+			$(".dot_5").css("background-color","gold");
+			$(".dot_6").css("background-color","gold");
+			$(".dot_7").css("background-color","gold");
+			$(".dot_8").css("background-color","gold");
+			$(".dot_9").css("background-color","gold");
+			$('.image_container_0').show();
+			$('.image_container_1').hide();
+			$('.image_container_2').hide();
+			$('.image_container_3').hide();
+			$('.image_container_4').hide();
+			$('.image_container_5').hide();
+			$('.image_container_6').hide();
+			$('.image_container_7').hide();
+			$('.image_container_8').hide();
+			$('.image_container_9').hide();
+			setTimeout(function(){ 
+				second_dot();
+  			}, 1010);
+		}
+	}
+	function second_dot(){
+  		if($('#images_main_container').has('image_container_1010')) {
+			$(".dot_0").css("background-color","gold");
+			$(".dot_1").css("background-color","black");
+			$(".dot_2").css("background-color","gold");
+			$(".dot_3").css("background-color","gold");
+			$(".dot_4").css("background-color","gold");
+			$(".dot_5").css("background-color","gold");
+			$(".dot_6").css("background-color","gold");
+			$(".dot_7").css("background-color","gold");
+			$(".dot_8").css("background-color","gold");
+			$(".dot_9").css("background-color","gold");
+			$('.image_container_0').hide();
+			$('.image_container_1').show();
+			$('.image_container_2').hide();
+			$('.image_container_3').hide();
+			$('.image_container_4').hide();
+			$('.image_container_5').hide();
+			$('.image_container_6').hide();
+			$('.image_container_7').hide();
+			$('.image_container_8').hide();
+			$('.image_container_9').hide();
+			setTimeout(function(){ 
+				third_dot();
+  			}, 1010);
+		}
+	}
+	function third_dot() {
+		if($('#images_main_container').has('image_container_1010')) {
+			$(".dot_0").css("background-color","gold");
+			$(".dot_1").css("background-color","gold");
+			$(".dot_2").css("background-color","black");
+			$(".dot_3").css("background-color","gold");
+			$(".dot_4").css("background-color","gold");
+			$(".dot_5").css("background-color","gold");
+			$(".dot_6").css("background-color","gold");
+			$(".dot_7").css("background-color","gold");
+			$(".dot_8").css("background-color","gold");
+			$(".dot_9").css("background-color","gold");
+			$('.image_container_0').hide();
+			$('.image_container_1').hide();
+			$('.image_container_2').show();
+			$('.image_container_3').hide();
+			$('.image_container_4').hide();
+			$('.image_container_5').hide();
+			$('.image_container_6').hide();
+			$('.image_container_7').hide();
+			$('.image_container_8').hide();
+			$('.image_container_9').hide();
+			setTimeout(function(){ 
+				fourth_dot();
+  			}, 1010);
+		}
+	}
+	function fourth_dot() {
+		if($('#images_main_container').has('image_container_1010')) {
+			$(".dot_0").css("background-color","gold");
+			$(".dot_1").css("background-color","gold");
+			$(".dot_2").css("background-color","gold");
+			$(".dot_3").css("background-color","black");
+			$(".dot_4").css("background-color","gold");
+			$(".dot_5").css("background-color","gold");
+			$(".dot_6").css("background-color","gold");
+			$(".dot_7").css("background-color","gold");
+			$(".dot_8").css("background-color","gold");
+			$(".dot_9").css("background-color","gold");
+			$('.image_container_0').hide();
+			$('.image_container_1').hide();
+			$('.image_container_2').hide();
+			$('.image_container_3').show();
+			$('.image_container_4').hide();
+			$('.image_container_5').hide();
+			$('.image_container_6').hide();
+			$('.image_container_7').hide();
+			$('.image_container_8').hide();
+			$('.image_container_9').hide();
+			setTimeout(function(){ 
+				fifth_dot();
+  			}, 1010);
+		}
+	}
+	function fifth_dot() {
+		if($('#images_main_container').has('image_container_1010')) {
+			$(".dot_0").css("background-color","gold");
+			$(".dot_1").css("background-color","gold");
+			$(".dot_2").css("background-color","gold");
+			$(".dot_3").css("background-color","gold");
+			$(".dot_4").css("background-color","black");
+			$(".dot_5").css("background-color","gold");
+			$(".dot_6").css("background-color","gold");
+			$(".dot_7").css("background-color","gold");
+			$(".dot_8").css("background-color","gold");
+			$(".dot_9").css("background-color","gold");
+			$('.image_container_0').hide();
+			$('.image_container_1').hide();
+			$('.image_container_2').hide();
+			$('.image_container_3').hide();
+			$('.image_container_4').show();
+			$('.image_container_5').hide();
+			$('.image_container_6').hide();
+			$('.image_container_7').hide();
+			$('.image_container_8').hide();
+			$('.image_container_9').hide();
+			setTimeout(function(){ 
+				sixth_dot();
+  			}, 1010);
+		}
+	}
+	function sixth_dot() {
+		if($('#images_main_container').has('image_container_1010')) {
+			$(".dot_0").css("background-color","gold");
+			$(".dot_1").css("background-color","gold");
+			$(".dot_2").css("background-color","gold");
+			$(".dot_3").css("background-color","gold");
+			$(".dot_4").css("background-color","gold");
+			$(".dot_5").css("background-color","black");
+			$(".dot_6").css("background-color","gold");
+			$(".dot_7").css("background-color","gold");
+			$(".dot_8").css("background-color","gold");
+			$(".dot_9").css("background-color","gold");
+			$('.image_container_0').hide();
+			$('.image_container_1').hide();
+			$('.image_container_2').hide();
+			$('.image_container_3').hide();
+			$('.image_container_4').hide();
+			$('.image_container_5').show();
+			$('.image_container_6').hide();
+			$('.image_container_7').hide();
+			$('.image_container_8').hide();
+			$('.image_container_9').hide();
+			setTimeout(function(){ 
+				seventh_dot();
+  			}, 1010);
+		}
+	}
+	function seventh_dot() {
+		if($('#images_main_container').has('image_container_1010')) {
+			$(".dot_0").css("background-color","gold");
+			$(".dot_1").css("background-color","gold");
+			$(".dot_2").css("background-color","gold");
+			$(".dot_3").css("background-color","gold");
+			$(".dot_4").css("background-color","gold");
+			$(".dot_5").css("background-color","gold");
+			$(".dot_6").css("background-color","black");
+			$(".dot_7").css("background-color","gold");
+			$(".dot_8").css("background-color","gold");
+			$(".dot_9").css("background-color","gold");
+			$('.image_container_0').hide();
+			$('.image_container_1').hide();
+			$('.image_container_2').hide();
+			$('.image_container_3').hide();
+			$('.image_container_4').hide();
+			$('.image_container_5').hide();
+			$('.image_container_6').show();
+			$('.image_container_7').hide();
+			$('.image_container_8').hide();
+			$('.image_container_9').hide();
+			setTimeout(function(){ 
+				eigth_dot();
+  			}, 1010);
+		}
+	}
+	function eigth_dot() {
+		if($('#images_main_container').has('image_container_1010')) {
+			$(".dot_0").css("background-color","gold");
+			$(".dot_1").css("background-color","gold");
+			$(".dot_2").css("background-color","gold");
+			$(".dot_3").css("background-color","gold");
+			$(".dot_4").css("background-color","gold");
+			$(".dot_5").css("background-color","gold");
+			$(".dot_6").css("background-color","gold");
+			$(".dot_7").css("background-color","black");
+			$(".dot_8").css("background-color","gold");
+			$(".dot_9").css("background-color","gold");
+			$('.image_container_0').hide();
+			$('.image_container_1').hide();
+			$('.image_container_2').hide();
+			$('.image_container_3').hide();
+			$('.image_container_4').hide();
+			$('.image_container_5').hide();
+			$('.image_container_6').hide();
+			$('.image_container_7').show();
+			$('.image_container_8').hide();
+			$('.image_container_9').hide();
+			setTimeout(function(){ 
+				ninth_dot();
+  			}, 1010);
+		}
+	}
+	function ninth_dot() {
+		if($('#images_main_container').has('image_container_1010')) {
+			$(".dot_0").css("background-color","gold");
+			$(".dot_1").css("background-color","gold");
+			$(".dot_2").css("background-color","gold");
+			$(".dot_3").css("background-color","gold");
+			$(".dot_4").css("background-color","gold");
+			$(".dot_5").css("background-color","gold");
+			$(".dot_6").css("background-color","gold");
+			$(".dot_7").css("background-color","gold");
+			$(".dot_8").css("background-color","black");
+			$(".dot_9").css("background-color","gold");
+			$('.image_container_0').hide();
+			$('.image_container_1').hide();
+			$('.image_container_2').hide();
+			$('.image_container_3').hide();
+			$('.image_container_4').hide();
+			$('.image_container_5').hide();
+			$('.image_container_6').hide();
+			$('.image_container_7').hide();
+			$('.image_container_8').show();
+			$('.image_container_9').hide();
+			setTimeout(function(){ 
+				tenth_dot();
+  			}, 1010);
+		}
+	}
+	function tenth_dot() {
+		if($('#images_main_container').has('image_container_1010')) {
+			$(".dot_0").css("background-color","gold");
+			$(".dot_1").css("background-color","gold");
+			$(".dot_2").css("background-color","gold");
+			$(".dot_3").css("background-color","gold");
+			$(".dot_4").css("background-color","gold");
+			$(".dot_5").css("background-color","gold");
+			$(".dot_6").css("background-color","gold");
+			$(".dot_7").css("background-color","gold");
+			$(".dot_8").css("background-color","gold");
+			$(".dot_9").css("background-color","black");
+			$('.image_container_0').hide();
+			$('.image_container_1').hide();
+			$('.image_container_2').hide();
+			$('.image_container_3').hide();
+			$('.image_container_4').hide();
+			$('.image_container_5').hide();
+			$('.image_container_6').hide();
+			$('.image_container_7').hide();
+			$('.image_container_8').hide();
+			$('.image_container_9').show();
+			setTimeout(function(){ 
+				first_dot();
+  			}, 1010);
+		}
+	}
+	//calling first_dot function to automate carousel.
+	first_dot();
+});
+</script>
+<style>
+  .images_main_container {
+    float: left;
+    width: 100%;
+    height: 400px;
+    background-color: #101010;
+    color: #FFFFFF;
+	position:relative;
+}
+  .bullets_main_container {
+		float: left;
+		width: 100%;
+		height: 40px;
+		background-color: #FFFFFF;
+		color: #FFFFFF;
+}
+  .bullets_main_container_3 {
+		float: left;
+		width: 100%;
+		height: 40px;
+		background-color: #CCCCCC;
+		color: #FFFFFF;
+}
+  .bullets_main_container_2 {
+		float: left;
+		width: 100%;
+		height: 40px;
+		background-color: #CCCCCC;
+		color: #FFFFFF;
+}
+  .bullets_main_container_1 {
+		float: left;
+		width: 100%;
+		height: 40px;
+		background-color: #CCCCCC;
+		color: #FFFFFF;
+}
+  .bullets_main_container_0 {
+		float: left;
+		width: 100%;
+		height: 40px;
+		background-color: #CCCCCC;
+		color: #FFFFFF;
+}
+  .image_container_0 {
+    float: left;
+    width: 100%;
+    height: 400px;
+    background-color: #101010;
+    color: #FFFFFF;
+    text-align: center;
+    margin-left: 0%;
+    background: url(Choosen/1010.gif) no-repeat center center fixed;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+	position:absolute;
+}
+  .image_container_1 {
+    float: left;
+    width: 100%;
+    height: 400px;
+    background-color: #101010;
+    color: #FFFFFF;
+    text-align: center;
+    margin-left: 0%;
+    background:url(Choosen/1.gif)no-repeat center center fixed;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+}
+  .image_container_2 {
+    float: left;
+    width: 100%;
+    height: 400px;
+    background-color: #101010;
+    color: #FFFFFF;
+    text-align: center;
+    margin-left: 0%;
+    background: url(Choosen/2.gif) no-repeat center center fixed;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+}
+  .image_container_3 {
+    float: left;
+    width: 100%;
+    height: 400px;
+    background-color: #101010;
+    color: #FFFFFF;
+    text-align: center;
+    margin-left: 0%;
+    background: url(Choosen/3.gif) no-repeat center center fixed;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+}
+  .image_container_4 {
+    float: left;
+    width: 100%;
+    height: 400px;
+    background-color: #101010;
+    color: #FFFFFF;
+    text-align: center;
+    margin-left: 0%;
+    background: url(Choosen/4.gif) no-repeat center center fixed;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+}
+  .image_container_5 {
+    float: left;
+    width: 100%;
+    height: 400px;
+    background-color: #101010;
+    color: #FFFFFF;
+    text-align: center;
+    margin-left: 0%;
+    background: url(Choosen/5.gif) no-repeat center center fixed;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+}
+  .image_container_6 {
+    float: left;
+    width: 100%;
+    height: 400px;
+    background-color: #101010;
+    color: #FFFFFF;
+    text-align: center;
+    margin-left: 0%;
+    background: url(Choosen/6.gif) no-repeat center center fixed;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+}
+  .image_container_7 {
+    float: left;
+    width: 100%;
+    height: 400px;
+    background-color: #101010;
+    color: #FFFFFF;
+    text-align: center;
+    margin-left: 0%;
+    background: url(Choosen/7.gif) no-repeat center center fixed;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+}
+  .image_container_8 {
+    float: left;
+    width: 100%;
+    height: 400px;
+    background-color: #101010;
+    color: #FFFFFF;
+    text-align: center;
+    margin-left: 0%;
+    background: url(Choosen/8.gif) no-repeat center center fixed;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+}
+  .image_container_9 {
+    float: left;
+    width: 100%;
+    height: 400px;
+    background-color: #101010;
+    color: #FFFFFF;
+    text-align: center;
+    margin-left: 0%;
+    background: url(Choosen/9.gif) no-repeat center center fixed;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+}
+  .dot_9 {
+	  height: 25px;
+	  width: 26px;
+	  background-color: #cccccc;
+	  border-radius: 50%;
+	  display: inline-block;
+	  float:left;
+	  text-align:center;
+	  margin-left:35%;
+	  margin-top:1%;
+}
+  .dot_8 {
+	  height: 25px;
+	  width: 26px;
+	  background-color: #cccccc;
+	  border-radius: 50%;
+	  display: inline-block;
+	  float:left;
+	  text-align:center;
+	  margin-left: 1%;
+	  margin-top:1%;
+}
+  .dot_7 {
+	  height: 25px;
+	  width: 26px;
+	  background-color: #cccccc;
+	  border-radius: 50%;
+	  display: inline-block;
+	  float:left;
+	  text-align:center;
+	  margin-left: 1%;
+	  margin-top:1%;
+}
+  .dot_6 {
+	  height: 25px;
+	  width: 26px;
+	  background-color: #cccccc;
+	  border-radius: 50%;
+	  display: inline-block;
+	  float:left;
+	  text-align:center;
+	  margin-left: 1%;
+	  margin-top:1%;
+}
+  .dot_5 {
+	  height: 25px;
+	  width: 26px;
+	  background-color: #cccccc;
+	  border-radius: 50%;
+	  display: inline-block;
+	  float:left;
+	  text-align:center;
+	  margin-left: 1%;
+	  margin-top:1%;
+}
+  .dot_4 {
+	  height: 25px;
+	  width: 26px;
+	  background-color: #cccccc;
+	  border-radius: 50%;
+	  display: inline-block;
+	  float:left;
+	  text-align:center;
+	  margin-left: 1%;
+	  margin-top:1%;
+}
+  .dot_3 {
+	  height: 25px;
+	  width: 26px;
+	  background-color: #cccccc;
+	  border-radius: 50%;
+	  display: inline-block;
+	  float:left;
+	  text-align:center;
+	  margin-left: 1%;
+	  margin-top:1%;
+}
+.dot_2 {
+	  height: 25px;
+	  width: 26px;
+	  background-color: #cccccc;
+	  border-radius: 50%;
+	  display: inline-block;
+	  float:left;
+	  text-align:center;
+	  margin-left: 1%;
+	  margin-top:1%;
+}
+.dot_1 {
+	  height: 25px;
+	  width: 26px;
+	  background-color: #cccccc;
+	  border-radius: 50%;
+	  display: inline-block;
+	  float:left;
+	  text-align:center;
+	  margin-left: 1%;
+	  margin-top:1%;
+}
+.dot_0 {
+	  height: 25px;
+	  width: 26px;
+	  background-color: #cccccc;
+	  border-radius: 50%;
+	  display: inline-block;
+	  float:left;
+	  text-align:center;
+	  margin-left: 1%;
+	  margin-top:1%;
+}
+</style>
+</head>
+<body>
+  <div class="images_main_container" id='images_main_container'>
+	  <div class='image_container_0' id="image_container_1010"></div>
+	  <div class='image_container_1' id="image_container_1010"></div>
+	  <div class='image_container_2' id="image_container_1010"></div>
+	  <div class='image_container_3' id="image_container_1010"></div>
+	  <div class='image_container_4' id="image_container_1010"></div>
+	  <div class='image_container_5' id="image_container_1010"></div>
+	  <div class='image_container_6' id="image_container_1010"></div>
+	  <div class='image_container_7' id="image_container_1010"></div>
+	  <div class='image_container_8' id="image_container_1010"></div>
+	  <div class='image_container_9' id="image_container_1010"></div>
+  </div>
+  <div class="bullets_main_container">
+	  <span class="dot_9"></span>
+	  <span class="dot_8"></span>
+	  <span class="dot_7"></span>
+	  <span class="dot_6"></span>
+	  <span class="dot_5"></span>
+	  <span class="dot_4"></span>
+	  <span class="dot_3"></span>
+	  <span class="dot_2"></span>
+	  <span class="dot_1"></span>
+	  <span class="dot_0"></span>
+  </div>
+</body>
+</html>
